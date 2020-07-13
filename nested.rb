64bc95fac357b113efd73d8b62e3094028cbc99c
Dani Bassett <dani@danithetallgirl.com>
2020-07-13 01:10:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+adding_to_dennis[:dennis_ritchie][:languages] << "Assembly"
 end
